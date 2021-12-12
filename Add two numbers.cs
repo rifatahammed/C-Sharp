10d1851d4bf5ed3c462972d7f1d2db01339ef28e
rifatahammed <reardj007@gmail.com>
2021-12-12 11:50:08 +0600

    Add Two Numbers

---
+++ Add two numbers.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Myaddition
+{
+    class AddTwoNumbers
+    {
+        static void Main(string[] args)
+        {
+            int x= 10;
+            int y=20;
+            int sum = x+y;
+            Console.WriteLine(sum);
+        }
+    }
+}
