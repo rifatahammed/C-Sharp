efe79d04099100890923aff2bcdd7fe2f4c8f7d9
rifatahammed <reardj007@gmail.com>
2022-01-15 23:25:11 +0600

    Recursion

---
+++ Factorial using recurtion.cs
@@ -0,0 +1,29 @@
+using System;    
+    
+namespace InterviewQuestionPart4    
+{    
+    class Program    
+    {    
+        static void Main(string[] args)    
+        {    
+            Console.WriteLine("Please Enter a Number");    
+    
+            //read number from user    
+            int number =Convert.ToInt32(Console.ReadLine());    
+    
+            //invoke the static method    
+            double factorial = Factorial(number);    
+    
+            //print the factorial result    
+            Console.WriteLine("factorial of"+number+"="+factorial.ToString());    
+    
+        }    
+        public static double Factorial(int number)    
+        {    
+            if (number == 0)    
+                return 1;    
+            return number * Factorial(number-1);//Recursive call    
+    
+        }    
+    }    
+}
